264b9efd9b27a8ac4a5e9f3c198545d64473d813
abhisek30 <abhisekcsce@gmail.com>
2021-12-02 22:32:52 +0530

    Base project

---
+++ app/src/main/java/com/asity/tech/native_android_designs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.asity.tech.native_android_designs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
